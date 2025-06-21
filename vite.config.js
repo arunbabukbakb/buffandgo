81e131949cb190babf5b7a7f61e18e284e2e3f9b
arunbabukbakb <arunbabukbakb@gmail.com>
2025-06-21 17:00:16 +0530

    pwa added, favicon changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true // allow PWA during development
+      },
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Buff & Go',
+        short_name: 'B&G',
+        description: 'We bring the shine, You stay inside',
+        theme_color: '#000000',         // Black to match the background
+        background_color: '#000000',
+        display: 'fullscreen',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
